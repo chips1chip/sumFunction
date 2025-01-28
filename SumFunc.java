57bced66aa2b03018dcd10411b21c8674a92dcca
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 01:44:00 +0530

    Update SumFunc.java

--- SumFunc.java
+++ SumFunc.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 public class SumFunc{
-public static void main(String[] args) {
-Scanner sc=new Scanner(System.in);
-int num1=sc.nextInt();
-int num2=sc.nextInt();
-int sum=add(num1, num2);
-System.out.println("the sum is "+ sum);
-sc.close();
-}
-public static int add(int num1, int num2){
-return num1+num2;
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();  // Read first number
+        int num2 = sc.nextInt();  // Read second number
+        int sum = add(num1, num2);  // Call add function to get sum
+        System.out.println("the sum is " + sum);  // Print the sum
+        sc.close();
+    }
+
+    // Function to add two numbers
+    public static int add(int num1, int num2){
+        return num1 + num2;  // Return the sum of num1 and num2
+    }
 }
